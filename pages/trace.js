4228d10e2d3085258d5c1053d77d90ad7b530736
Sean-Grobicki <seangrobicki@gmail.com>
2021-01-13 09:51:04 +0000

    correcting linking errors and import errors

--- pages/trace.js
+++ pages/trace.js
@@ -1,26 +1,29 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Trace() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Trace Question</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
         <div id ="container">
             <header>
-            <h1>Grammar Question</h1>
+            <h1>Trace Question</h1>
             <nav> 
-                <a href = "grammar.js"><button type = "button" >Grammar</button></a>
-                <a href = "programming.php"><button type ="button" >Programming</button></a>
-                <a href = "trace.php"><button type = "button" >Trace</button></a>
+                <Link href = "grammar"><button type = "button" >Grammar</button></Link>
+                <Link href = "programming"><button type ="button" >Programming</button></Link>
+                <Link href = "trace"><button type = "button" >Trace</button></Link>
             </nav>
             </header>
+            
             <article id = "question">
-            <h2>Fix the grammatical errors that are present in the following code. </h2>
-            <textarea rows= "30"> 
+            <h2>Fill in the trace table below to predict the outcome of the program.</h2>
+            <textarea rows= "30">
+
             </textarea>
             
             
